f0198a735de219a5d7db2bbe3f1f6288f435b5c2
Team 4796 <no@email.com>
2017-02-20 23:29:27 -0500

    Add physics support

---
+++ robot/physics.py
@@ -0,0 +1,64 @@
+#
+# See the documentation for more details on how this works
+#
+# The idea here is you provide a simulation object that overrides specific
+# pieces of WPILib, and modifies motors/sensors accordingly depending on the
+# state of the simulation. An example of this would be measuring a motor
+# moving for a set period of time, and then changing a limit switch to turn 
+# on after that period of time. This can help you do more complex simulations
+# of your robot code without too much extra effort.
+#
+# NOTE: THIS API IS ALPHA AND WILL MOST LIKELY CHANGE!
+#       ... if you have better ideas on how to implement, submit a patch!
+#
+
+from pyfrc.physics import drivetrains
+
+from components.drive import Drive
+
+
+class PhysicsEngine(object):
+    '''
+        Simulates a motor moving something that strikes two limit switches,
+        one on each end of the track. Obviously, this is not particularly
+        realistic, but it's good enough to illustrate the point
+    '''
+    
+    def __init__(self, physics_controller):
+        '''
+            :param physics_controller: `pyfrc.physics.core.PhysicsInterface` object
+                                       to communicate simulation effects to
+        '''
+        
+        self.physics_controller = physics_controller
+        self.position = 0
+        
+        self.ft_per_sec = 5
+        
+        self.physics_controller.add_device_gyro_channel('navxmxp_spi_4_angle')
+            
+    def update_sim(self, hal_data, now, tm_diff):
+        '''
+            Called when the simulation parameters for the program need to be
+            updated.
+            
+            :param now: The current time as a float
+            :param tm_diff: The amount of time that has passed since the last
+                            time that this function was called
+        '''
+        
+        # Simulate the drivetrain
+        l_motor = -hal_data['pwm'][0]['value']
+        r_motor = -hal_data['pwm'][1]['value']
+        
+        speed, rotation = drivetrains.two_motor_drivetrain(l_motor, r_motor, speed=self.ft_per_sec)
+        self.physics_controller.drive(speed, rotation, tm_diff)
+        
+        # Inches we traveled
+        distance_inches = 12.0*speed*tm_diff
+        
+        # Use that to give a rough approximation of encoder values.. not
+        # accurate for turns, but we don't need that
+        # -> encoder = distance / (18.85 / 360.0)
+        
+        hal_data['encoder'][0]['count'] += int(distance_inches / (Drive.circumference/360.0))
