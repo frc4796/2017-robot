224470329473315236cf1c48c17e6b065ec107f4
KDiaz1 <kevinjosediaz9@gmail.com>
2017-02-21 17:29:03 -0500

    Made it work on real robot

--- robot/robot.py
+++ robot/robot.py
@@ -9,31 +9,39 @@ from components.climber import Climber
 from components.gears import Gears
 
 class MyRobot(MagicRobot):
-    
+
     #
     # Define components here
     #
-    
+
     drive = Drive
     climber = Climber
     gears = Gears
-    
-    
+
+    practice_robot = False
+
+
     def createObjects(self):
         """Initialize all wpilib motors & sensors"""
-        
-        left = wpilib.Spark(0)
-        right = wpilib.Spark(1)
-        right.setInverted(True)
-        left.setInverted(True)
-        
+
+        if self.practice_robot:
+            left = wpilib.Spark(0)
+            right = wpilib.Spark(1)
+            right.setInverted(True)
+            left.setInverted(True)
+        else:
+            left = wpilib.TalonSRX(1)
+            right = wpilib.TalonSRX(0)
+            right.setInverted(True)
+            left.setInverted(True)
+
         self.robotdrive = wpilib.RobotDrive(left, right)
         #self.xboxcontroller = wpilib.XboxController(0)
         self.leftdoormotor = wpilib.Spark(2)
         self.rightdoormotor = wpilib.Spark(3)
         self.encoder = wpilib.Encoder(0,1)
         self.stick = wpilib.Joystick(0)
-        
+
     def teleopPeriodic(self):
         """Place code here that does things as a result of operator
            actions"""
@@ -42,6 +50,6 @@ class MyRobot(MagicRobot):
         #if self.xboxcontroller.getBumper(self.xboxcontroller.Hand.kRight):
         #    self.gears.close()
         self.drive.move(self.stick.getY(), self.stick.getX())
-            
+
 if __name__ == '__main__':
     wpilib.run(MyRobot)
